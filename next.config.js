a9fb1552c38c1ecf5741da45f4ab50cf5a024723
unknown <jacob.e.england@gmail.com>
2020-12-07 23:46:39 -0500

    Working through next.js tutorial in this dir, going to modify into the eventual app

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+
+dotenvLoad()
+
+const withNextEnv = nextEnv()
+module.exports = withNextEnv()
---
+++ pages/notes/[id].jsx
@@ -0,0 +1,18 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const Page = () => {
+    const router = useRouter()
+    const { id }= router.query
+  
+    return (
+      <div sx={{variant: 'containers.page'}}>
+        <h1>Note: {id} </h1>
+      </div>
+    )
+}
+
+export default Page
